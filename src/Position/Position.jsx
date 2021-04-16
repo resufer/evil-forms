be875732112a637adccf47b347edd15ea57a35b3
resufer <66846932+resufer@users.noreply.github.com>
2021-04-16 22:23:22 +0300

    Update Position.jsx

--- src/Position/Position.jsx
+++ src/Position/Position.jsx
@@ -3,8 +3,16 @@ import React from 'react';
 
 function Position() {
   return (
-    <div>
-      choice Position
+    <div className='position'>
+      <select size='1' required>
+        <option>HR Specialist</option>
+        <option>Junior FrontEnd</option>
+        <option>Junior BackEnd</option>
+        <option>Middle FrontEnd</option>
+        <option>Middle BackEnd</option>
+        <option>Senior FrontEnd</option>
+        <option>Senior BackEnd</option>
+      </select>
     </div>
   )
 };
