5f3bf12ed30e7080212688e3f91beae08e2745a2
resufer <66846932+resufer@users.noreply.github.com>
2021-04-17 12:10:31 +0300

    Update Position.jsx

--- src/Position/Position.jsx
+++ src/Position/Position.jsx
@@ -5,6 +5,7 @@ function Position() {
   return (
     <div className='position'>
       <select size='1' required>
+        <option disabled selected>Select your position</option>
         <option>HR Specialist</option>
         <option>Junior FrontEnd</option>
         <option>Junior BackEnd</option>
