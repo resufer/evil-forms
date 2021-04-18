8a9539e1f2864984acc4f497e8f86bb2a37d4820
resufer <66846932+resufer@users.noreply.github.com>
2021-04-18 23:40:13 +0300

    Update Name.jsx

--- src/Name/Name.jsx
+++ src/Name/Name.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 
 
-function Name() {
-  return (
-    <div className='inputName'>
-      <input placeholder='Your name' />
-    </div>
-  )
+class Name extends React.Component {
+  state = {
+    errorCondition: false
+  }
+
+  componentDidMount() {
+    let name = document.getElementById('name');
+    name.addEventListener('keyup', (e) => {
+      if (!((e.key.codePointAt() >= 65 && e.key.codePointAt() <= 90)
+        || (e.key.codePointAt() >= 97 && e.key.codePointAt() <= 122))) {
+        this.setState({ errorCondition: true });
+        name.value = name.value.slice(0, name.value.length - 1)
+        setTimeout(() => {
+          this.setState({ errorCondition: false });
+        }, 2000)
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className='inputName'>
+        {this.state.errorCondition && <div className='alert'>You can use only a-Z</div>}
+        <input placeholder='Your name' id='name' autocomplete="off" />
+      </div>
+    )
+  }
 };
 
 export default Name;
