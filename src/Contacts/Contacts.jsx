66c7f278c46a1de2dd0dd2efc42d38620bdcaf50
resufer <66846932+resufer@users.noreply.github.com>
2021-04-18 23:41:05 +0300

    Update Contacts.jsx

--- src/Contacts/Contacts.jsx
+++ src/Contacts/Contacts.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 
 
-function Contacts() {
-  return (
-    <div className='contacts'>
-      <input type="email" placeholder='Your email' />
-    </div>
-  )
+class Contacts extends React.Component {
+  state = {
+    errorCondition: false
+  }
+
+  componentDidMount() {
+    let email = document.getElementById('email');
+    email.addEventListener('keyup', (e) => {
+      if (!((e.key.codePointAt() >= 65 && e.key.codePointAt() <= 90)
+        || (e.key.codePointAt() >= 97 && e.key.codePointAt() <= 122)
+        || (e.key.codePointAt() >= 48 && e.key.codePointAt() <= 57)
+        || e.key === '@' || e.key === '.')) {
+        this.setState({ errorCondition: true });
+        email.value = email.value.slice(0, email.value.length - 1);
+        setTimeout(() => {
+          this.setState({ errorCondition: false });
+        }, 2000)
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className='contacts'>
+        {this.state.errorCondition && <div className='alert'>Enter a valid email address</div>}
+        <input type="email" placeholder='Your email' id='email' />
+      </div>
+    )
+  }
+
 };
 
 export default Contacts;
