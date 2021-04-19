d2a0e8b855f8538e3997770e99c6de415a705ed7
resufer <66846932+resufer@users.noreply.github.com>
2021-04-19 21:45:43 +0300

    Update Surname.jsx

--- src/Surname/Surname.jsx
+++ src/Surname/Surname.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 class Surname extends React.Component {
   state = {
-    errorCondition: false
+    errorCondition: false,
+    angryCount: 0
   }
 
   componentDidMount() {
     let surname = document.getElementById('surname');
+
     surname.addEventListener('keyup', (e) => {
       if (!((e.key.codePointAt() >= 65 && e.key.codePointAt() <= 90)
         || (e.key.codePointAt() >= 97 && e.key.codePointAt() <= 122))) {
@@ -16,14 +18,23 @@ class Surname extends React.Component {
           this.setState({ errorCondition: false });
         }, 2000)
       }
-    })
+    });
+
+    surname.addEventListener('blur', () => {
+      if (surname.value && !this.state.angryCount) {
+        this.props.setAngry(this.props.angry + 2);
+        this.setState({ angryCount: this.state.angryCount + 1 })
+      } else if (surname.value && this.state.angryCount) {
+        this.setState({ angryCount: this.state.angryCount + 1 })
+      }
+    });
   }
 
   render() {
     return (
       <div className='inputSurname'>
         {this.state.errorCondition && <div className='alert'>You can use only a-Z</div>}
-        <input placeholder='Your surname' id='surname' autocomplete="off" />
+        <input placeholder='Your surname' id='surname' autocomplete="off" disabled={this.state.angryCount === 3} />
       </div>
     )
   }
