cdc3305b1a55af0e0ed1d813befbeac61b302e5b
resufer <66846932+resufer@users.noreply.github.com>
2021-04-19 21:46:51 +0300

    Update Contacts.jsx

--- src/Contacts/Contacts.jsx
+++ src/Contacts/Contacts.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 
 class Contacts extends React.Component {
   state = {
-    errorCondition: false
+    errorCondition: false,
+    angryCount: 0
   }
 
   componentDidMount() {
     let email = document.getElementById('email');
+
     email.addEventListener('keyup', (e) => {
       if (!((e.key.codePointAt() >= 65 && e.key.codePointAt() <= 90)
         || (e.key.codePointAt() >= 97 && e.key.codePointAt() <= 122)
@@ -19,14 +21,23 @@ class Contacts extends React.Component {
           this.setState({ errorCondition: false });
         }, 2000)
       }
-    })
+    });
+
+    email.addEventListener('blur', () => {
+      if (email.value && !this.state.angryCount) {
+        this.props.setAngry(this.props.angry + 2);
+        this.setState({ angryCount: this.state.angryCount + 1 })
+      } else if (email.value && this.state.angryCount) {
+        this.setState({ angryCount: this.state.angryCount + 1 })
+      }
+    });
   }
 
   render() {
     return (
       <div className='contacts'>
         {this.state.errorCondition && <div className='alert'>Enter a valid email address</div>}
-        <input type="email" placeholder='Your email' id='email' />
+        <input type="email" placeholder='Your email' id='email' disabled={this.state.angryCount === 3} />
       </div>
     )
   }
