8e151462cc236ee1a00879fc984246054bc39851
resufer <66846932+resufer@users.noreply.github.com>
2021-04-19 21:44:27 +0300

    Update Descriptions.jsx

--- src/Descriptions/Descriptions.jsx
+++ src/Descriptions/Descriptions.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 
 
-function Descriptions() {
-  return (
-    <div className='descriptions'>
-      <textarea placeholder='describe the situation'></textarea>
-    </div>
-  )
+class Descriptions extends React.Component {
+  state = {
+    wordCount: 0,
+    angryCount: 0
+  }
+
+  componentDidMount() {
+    let descriptions = document.getElementById('descriptions');
+
+    descriptions.addEventListener('keyup', (e) => {
+      this.setState({ wordCount: descriptions.value.split(' ').length });
+    });
+
+    descriptions.addEventListener('blur', () => {
+      if (descriptions.value && this.state.wordCount > 4) {
+        setInterval(() => {
+          this.setState({ angryCount: 3 });
+          this.props.setAngry(this.state.wordCount / 4);
+        }, 5000)
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className='descriptions'>
+        <textarea placeholder='describe the situation' id='descriptions' disabled={this.state.angryCount === 3}></textarea>
+      </div>
+    )
+  }
 };
 
 export default Descriptions;
