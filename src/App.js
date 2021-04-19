d7e73145a62c2c3ca340ab32d1c328b1bd47dc63
resufer <66846932+resufer@users.noreply.github.com>
2021-04-19 21:43:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ import Position from "./Position/Position";
 import Surname from "./Surname/Surname";
 
 function App() {
+  let [angry, setAngry] = React.useState(0);
+
   return (
     <div className='wrapper'>
       <div className='general'>
         <div className='general__title'>Filing a complaint</div>
         <div className='form'>
           <div className='inputs'>
-            <Name />
-            <Surname />
-            <Position />
-            <Contacts />
+            <Name angry={angry} setAngry={setAngry} />
+            <Surname angry={angry} setAngry={setAngry} />
+            <Position angry={angry} setAngry={setAngry} />
+            <Contacts angry={angry} setAngry={setAngry} />
           </div>
           <div className='descriptionArea'>
-            <Descriptions />
+            <Descriptions angry={angry} setAngry={setAngry} />
           </div>
         </div>
         <div className='send'>
