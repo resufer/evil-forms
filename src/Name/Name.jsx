e1634b4236b656924e055961188d1633b800a1dd
resufer <66846932+resufer@users.noreply.github.com>
2021-04-19 21:45:15 +0300

    Update Name.jsx

--- src/Name/Name.jsx
+++ src/Name/Name.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 
 class Name extends React.Component {
   state = {
-    errorCondition: false
+    errorCondition: false,
+    angryCount: 0
   }
 
   componentDidMount() {
     let name = document.getElementById('name');
+
     name.addEventListener('keyup', (e) => {
       if (!((e.key.codePointAt() >= 65 && e.key.codePointAt() <= 90)
         || (e.key.codePointAt() >= 97 && e.key.codePointAt() <= 122))) {
@@ -17,14 +19,23 @@ class Name extends React.Component {
           this.setState({ errorCondition: false });
         }, 2000)
       }
-    })
+    });
+
+    name.addEventListener('blur', () => {
+      if (name.value && !this.state.angryCount) {
+        this.props.setAngry(this.props.angry + 2);
+        this.setState({ angryCount: this.state.angryCount + 1 })
+      } else if (name.value && this.state.angryCount) {
+        this.setState({ angryCount: this.state.angryCount + 1 })
+      }
+    });
   }
 
   render() {
     return (
       <div className='inputName'>
         {this.state.errorCondition && <div className='alert'>You can use only a-Z</div>}
-        <input placeholder='Your name' id='name' autocomplete="off" />
+        <input placeholder='Your name' id='name' autocomplete="off" disabled={this.state.angryCount === 3} />
       </div>
     )
   }
