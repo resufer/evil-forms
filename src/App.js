252ebcb2ec848ca9285184259fba3e4b42d00f39
resufer <66846932+resufer@users.noreply.github.com>
2021-04-21 18:09:14 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,30 +5,38 @@ import Name from "./Name/Name";
 import Position from "./Position/Position";
 import Surname from "./Surname/Surname";
 
-function App() {
-  let [angry, setAngry] = React.useState(0);
+class App extends React.Component {
+  state = {
+    angry: 0
+  }
 
-  return (
-    <div className='wrapper'>
-      <div className='general'>
-        <div className='general__title'>Filing a complaint</div>
-        <div className='form'>
-          <div className='inputs'>
-            <Name angry={angry} setAngry={setAngry} />
-            <Surname angry={angry} setAngry={setAngry} />
-            <Position angry={angry} setAngry={setAngry} />
-            <Contacts angry={angry} setAngry={setAngry} />
+  setAngry(angry) {
+    this.setState({ angry: angry })
+  }
+
+  render() {
+    return (
+      <div className='wrapper'>
+        <div className='general'>
+          <div className='general__title'>Filing a complaint</div>
+          <div className='form'>
+            <div className='inputs'>
+              <Name angry={this.state.angry} setAngry={this.setAngry.bind(this)} />
+              <Surname angry={this.state.angry} setAngry={this.setAngry.bind(this)} />
+              <Position angry={this.state.angry} setAngry={this.setAngry.bind(this)} />
+              <Contacts angry={this.state.angry} setAngry={this.setAngry.bind(this)} />
+            </div>
+            <div className='descriptionArea'>
+              <Descriptions angry={this.state.angry} setAngry={this.setAngry.bind(this)} />
+            </div>
           </div>
-          <div className='descriptionArea'>
-            <Descriptions angry={angry} setAngry={setAngry} />
+          <div className='send'>
+            <button disabled={this.state.angry < 10}>Send</button>
           </div>
-        </div>
-        <div className='send'>
-          <button>Send</button>
         </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
