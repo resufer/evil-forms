26a2777765fd7469b65647c9a3246caa00adbfbc
resufer <66846932+resufer@users.noreply.github.com>
2021-05-09 20:08:12 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
 import React from 'react';
 import Contacts from "./Contacts/Contacts";
 import Descriptions from "./Descriptions/Descriptions";
+import { End } from './End/End';
 import Name from "./Name/Name";
 import Position from "./Position/Position";
+import { runaway } from './runaway';
 import Surname from "./Surname/Surname";
 
 class App extends React.Component {
   state = {
-    angry: 0
+    angry: 0,
   }
 
   setAngry(angry) {
-    this.setState({ angry: angry })
+    this.setState({ angry: this.state.angry + angry })
+  }
+
+  componentDidMount() {
+    runaway(this.setAngry.bind(this), this.state.name, this.state.surname);
   }
 
   componentDidUpdate() {
-    let wrapper = document.querySelector('.wrapper');
-    wrapper.style.backgroundColor = `rgb(
-      ${255 - this.state.angry * 10},
-      ${255 - this.state.angry * 10},
-      ${255 - this.state.angry * 10}
-      )`
+    if (this.state.angry < 10000) {
+      let wrapper = document.querySelector('.wrapper');
+      wrapper.style.backgroundColor = `rgb(
+      ${0 + this.state.angry * 5},
+      ${0}, ${0})`;
+    }
   }
 
   render() {
+    if (this.state.angry >= 10000) {
+      return <End />
+    }
+ 
     return (
       <div className='wrapper'>
         <div className='general'>
           <div className='general__title'>Filing a complaint</div>
           <div className='form'>
             <div className='inputs'>
-              <Name angry={this.state.angry} setAngry={this.setAngry.bind(this)} />
-              <Surname angry={this.state.angry} setAngry={this.setAngry.bind(this)} />
-              <Position angry={this.state.angry} setAngry={this.setAngry.bind(this)} />
-              <Contacts angry={this.state.angry} setAngry={this.setAngry.bind(this)} />
+              <Name setAngry={this.setAngry.bind(this)}/>
+              <Surname setAngry={this.setAngry.bind(this)}/>
+              <Position setAngry={this.setAngry.bind(this)} />
+              <Contacts setAngry={this.setAngry.bind(this)} />
             </div>
             <div className='descriptionArea'>
               <Descriptions angry={this.state.angry} setAngry={this.setAngry.bind(this)} />
@@ -41,6 +51,9 @@ class App extends React.Component {
           </div>
           <div className='send'>
             <button disabled={this.state.angry < 10}>Send</button>
+            <button className='hidden' onClick={() => { window.location.reload()}}>
+              clear!!!
+            </button>
           </div>
         </div>
       </div>
